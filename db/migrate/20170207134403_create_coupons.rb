b2839b0e9b40845202ad5b435f4ca624b0b8e4e2
Denis Pavlov <pdv.spb@gmail.com>
2017-02-07 18:14:41 +0300

    table patch

--- db/migrate/20170207134403_create_coupons.rb
+++ db/migrate/20170207134403_create_coupons.rb
@@ -1,7 +1,7 @@
 class CreateCoupons < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |t|
-      t.belongs_to :user
+      t.belongs_to :deal
       t.boolean :actual, default: false, null: false
 
       t.timestamps
